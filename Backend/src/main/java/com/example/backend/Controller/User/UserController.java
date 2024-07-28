d45190e29fc14d3e2f0748c0c75f8fb804c22b8f
Phạm Quốc Hưng <96472052+quochung2k3@users.noreply.github.com>
2024-07-28 07:41:54 +0700

    API

--- Backend/src/main/java/com/example/backend/Controller/User/UserController.java
+++ Backend/src/main/java/com/example/backend/Controller/User/UserController.java
@@ -3,19 +3,17 @@ package com.example.backend.Controller.User;
 import com.example.backend.Model.SANPHAM;
 import com.example.backend.Service.SANPHAMService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
-@Controller
+@RestController
 public class UserController {
     @Autowired
     SANPHAMService sanphamService;
-    @RequestMapping("/")
-    public String index(Model model) {
-//        List<SANPHAM> sanPham = sanphamService.findAll();
-        System.out.println("Test");
-        return "user/index";
+
+    @GetMapping("/api/products")
+    public List<SANPHAM> getAllProducts() {
+        return sanphamService.findAll();
     }
 }
